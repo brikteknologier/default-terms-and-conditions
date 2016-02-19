8b9348e14457688d63f5d53d5d8f848365c94a5e
Jon Packer <contact@jonpacker.com>
2016-02-19 11:28:49 +1100

    fix build script

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-fs.writeFileSync(JSON.stringify({
+fs.writeFileSync('terms-and-conditions.json', JSON.stringify({
   defaultTermsOfService: fs.readFileSync('./terms-of-service.md', 'utf8'),
   defaultPrivacyPolicy: fs.readFileSync('./privacy-policy.md', 'utf8')
-}, 'utf8');
+}), 'utf8');
